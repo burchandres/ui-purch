3493e2c75f6eac8759f80b23cd253396c70e569a
sereno <serenomdominguez@gmail.com>
2025-08-03 14:56:26 -0400

    standardizing imports

--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -1,68 +1,70 @@
 import {
-	Sidebar,
-	SidebarContent,
-	SidebarFooter,
-	SidebarGroup,
-	SidebarHeader,
-	SidebarMenu,
-	SidebarMenuButton,
-} from '@/components/base/sidebar';
-import { PanelLeftClose } from 'lucide-react';
-import { PurchLogoSquare } from '@/components/icons/purch-logo';
-import { Link } from '@tanstack/react-router';
-import { pages } from '../../config/pages';
-import { useSidebar } from '../base/sidebar';
-import { Button } from '../base/button';
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+} from "@/components/base/sidebar";
+import { PanelLeftClose } from "lucide-react";
+import { PurchLogoSquare } from "@/components/icons/purch-logo";
+import { Link } from "@tanstack/react-router";
+import { pages } from "@/config/pages";
+import { useSidebar } from "@/components/base/sidebar";
+import { Button } from "@/components/base/button";
 
 export function NavBar() {
-	const { open, isMobile, openMobile, toggleSidebar } = useSidebar();
+  const { open, isMobile, openMobile, toggleSidebar } = useSidebar();
 
-	const openOrMobile = open || (isMobile && openMobile);
+  const openOrMobile = open || (isMobile && openMobile);
 
-	return (
-		<Sidebar
-			collapsible="icon"
-			onClick={(e) => {
-				if (!(e.target as HTMLElement).closest('button') && !openOrMobile)
-					toggleSidebar();
-			}}
-		>
-			<SidebarHeader>
-				<Link to="/">
-					{openOrMobile ? (
-						<div className="flex ml-1 mt-2 justify-between items-center">
-							<div className="flex items-center h-8 group cursor-pointer w-fit">
-								<PurchLogoSquare color="black" size="27" />
-								<span className="logo-text font-bold text-xl mb-1 ml-2">
-									Purch
-								</span>
-							</div>
-							<Button size="sm" variant="ghost" onClick={toggleSidebar}>
-								<PanelLeftClose />
-							</Button>
-						</div>
-					) : (
-						<div className="flex ml-1 items-center mt-2 h-8">
-							<PurchLogoSquare color="black" size="27" />
-						</div>
-					)}
-				</Link>
-			</SidebarHeader>
-			<SidebarContent>
-				<SidebarGroup>
-					<SidebarMenu>
-						{pages.map((page) => (
-							<Link key={page.url} to={page.url} className="navbar-item">
-								<SidebarMenuButton tooltip={!openOrMobile ? page.display : ''}>
-									{page.icon && <page.icon />}
-									<span className="navbar-item">{page.display}</span>
-								</SidebarMenuButton>
-							</Link>
-						))}
-					</SidebarMenu>
-				</SidebarGroup>
-			</SidebarContent>
-			<SidebarFooter />
-		</Sidebar>
-	);
+  return (
+    <Sidebar
+      collapsible="icon"
+      onClick={(e) => {
+        if (!(e.target as HTMLElement).closest("button") && !openOrMobile)
+          toggleSidebar();
+      }}
+    >
+      <SidebarHeader>
+        {openOrMobile ? (
+          <div className="flex ml-1 mt-2 justify-between items-center">
+            <Link to="/">
+              <div className="flex items-center h-8 group cursor-pointer w-fit">
+                <PurchLogoSquare color="black" size="27" />
+                <span className="logo-text font-bold text-xl mb-1 ml-2">
+                  Purch
+                </span>
+              </div>
+            </Link>
+            <Button size="sm" variant="ghost" onClick={toggleSidebar}>
+              <PanelLeftClose />
+            </Button>
+          </div>
+        ) : (
+          <Link to="/">
+            <div className="flex ml-1 items-center mt-2 h-8">
+              <PurchLogoSquare color="black" size="27" />
+            </div>
+          </Link>
+        )}
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {pages.map((page) => (
+              <Link key={page.url} to={page.url} className="navbar-item">
+                <SidebarMenuButton tooltip={!openOrMobile ? page.display : ""}>
+                  {page.icon && <page.icon />}
+                  <span className="navbar-item">{page.display}</span>
+                </SidebarMenuButton>
+              </Link>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
 }
---
+++ src/lib/shadcn.ts
@@ -0,0 +1,7 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+// used by shadcn
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
