34e476847bb0823c7581bc6036c8b3fb7c4a86cc
sereno <serenomdominguez@gmail.com>
2025-08-03 13:04:25 -0400

    improved navbar

--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -1,53 +1,67 @@
 import {
-	Sidebar,
-	SidebarContent,
-	SidebarFooter,
-	SidebarGroup,
-	SidebarHeader,
-	SidebarMenu,
-	SidebarMenuButton,
-} from '@/components/base/sidebar';
-import { PurchLogoSquare } from '@/components/icons/purch-logo';
-import { Link } from '@tanstack/react-router';
-import { pages } from '../../config/pages';
-import { useSidebar } from '../base/sidebar';
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+} from "@/components/base/sidebar";
+import { PanelLeftClose, PanelLeftOpen } from "lucide-react";
+import { PurchLogoSquare } from "@/components/icons/purch-logo";
+import { Link } from "@tanstack/react-router";
+import { pages } from "../../config/pages";
+import { useSidebar } from "../base/sidebar";
+import { Button } from "../base/button";
 
 export function NavBar() {
-	const { open } = useSidebar();
+  const { open, isMobile, openMobile, toggleSidebar } = useSidebar();
 
-	return (
-		<Sidebar collapsible="icon">
-			<SidebarHeader>
-				<Link to="/">
-					{open ? (
-						<div className="flex items-center ml-5 mt-2">
-							<PurchLogoSquare color="black" size="35" />
-							<span className="logo-text font-bold text-xl mb-1 ml-2">
-								Purch
-							</span>
-						</div>
-					) : (
-						<div className="flex items-center justify-center ml-1 mt-1">
-							<PurchLogoSquare color="black" size="26" />
-						</div>
-					)}
-				</Link>
-			</SidebarHeader>
-			<SidebarContent>
-				<SidebarGroup>
-					<SidebarMenu>
-						{pages.map((page) => (
-							<Link key={page.url} to={page.url} className="navbar-item">
-								<SidebarMenuButton>
-									{page.icon && <page.icon />}
-									<span className="navbar-item">{page.display}</span>
-								</SidebarMenuButton>
-							</Link>
-						))}
-					</SidebarMenu>
-				</SidebarGroup>
-			</SidebarContent>
-			<SidebarFooter />
-		</Sidebar>
-	);
+  const openOrMobile = open || (isMobile && openMobile);
+
+  return (
+    <Sidebar
+      collapsible="icon"
+      onClick={(e) => {
+        if (!e.target.closest("button") && !openOrMobile) toggleSidebar();
+      }}
+    >
+      <SidebarHeader>
+        <Link to="/">
+          {openOrMobile ? (
+            <div className="flex ml-1 mt-2 justify-between items-center">
+              <div className="flex items-center h-8 group cursor-pointer w-fit">
+                <PurchLogoSquare color="black" size="27" />
+                <span className="logo-text font-bold text-xl mb-1 ml-2">
+                  Purch
+                </span>
+              </div>
+              <Button size="sm" variant="ghost" onClick={toggleSidebar}>
+                <PanelLeftClose />
+              </Button>
+            </div>
+          ) : (
+            <div className="flex ml-1 items-center mt-2 h-8">
+              <PurchLogoSquare color="black" size="27" />
+            </div>
+          )}
+        </Link>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {pages.map((page) => (
+              <Link key={page.url} to={page.url} className="navbar-item">
+                <SidebarMenuButton tooltip={!openOrMobile ? page.display : ""}>
+                  {page.icon && <page.icon />}
+                  <span className="navbar-item">{page.display}</span>
+                </SidebarMenuButton>
+              </Link>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
 }
