6de658fc491ff8cf2889fe8949f161ef1e2dfe84
sereno <serenomdominguez@gmail.com>
2025-08-03 22:21:59 -0400

    minor fixes

--- src/lib/authLoader.ts
+++ src/lib/authLoader.ts
@@ -1,9 +1,9 @@
 import { redirect } from "@tanstack/react-router";
 import { getCurrentUser } from "@/lib/api/user";
 
+// acts as a gateway to inform routing loaders whether to redirect or not
 export async function authLoader() {
   const user = await getCurrentUser();
-  console.log("user", user);
   if (!user) throw redirect({ to: "/" });
   return { user };
 }
