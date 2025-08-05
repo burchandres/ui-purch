8f789acd90c962401f4eb567a8280d2954e1b36e
sereno <serenomdominguez@gmail.com>
2025-08-05 01:09:43 -0400

    remove tanstack dev tools. messing with the vibes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { queryClient } from "@/lib/queryClient";
 import { Toaster } from "@/components/base/sonner";
 
@@ -12,7 +12,9 @@ export const Route = createRootRoute({
         <Toaster />
         <Outlet />
       </QueryClientProvider>
-      <TanStackRouterDevtools initialIsOpen={false} />
+      {/*
+        <TanStackRouterDevtools initialIsOpen={false} />
+      */}
     </>
   ),
 });
