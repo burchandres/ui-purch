cd77b02b2b3285e6f145292148201af2320fdd72
sereno <serenomdominguez@gmail.com>
2025-08-04 22:43:49 -0400

    added sonner and shadcn wrapper

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,18 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-
-const queryClient = new QueryClient();
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { queryClient } from "@/lib/queryClient";
+import { Toaster } from "@/components/base/sonner";
 
 export const Route = createRootRoute({
-	component: () => (
-		<>
-			<QueryClientProvider client={queryClient}>
-				<main />
-			</QueryClientProvider>
-			<Outlet />
-			<TanStackRouterDevtools initialIsOpen={false} />
-		</>
-	),
+  component: () => (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <main />
+        <Toaster />
+        <Outlet />
+      </QueryClientProvider>
+      <TanStackRouterDevtools initialIsOpen={false} />
+    </>
+  ),
 });
