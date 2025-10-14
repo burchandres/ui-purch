2c569eacfedf888581242a68f8e7d7159df3069b
sereno <serenomdominguez@gmail.com>
2025-10-14 01:33:42 -0400

    edit acct -- need to fix password

--- src/components/settings/account-settings.tsx
+++ src/components/settings/account-settings.tsx
@@ -1,6 +1,25 @@
-import type { FC } from 'react';
-import { CreateAccountCard } from '../landing/user-forms/create-account';
+import { type FC, useEffect } from 'react';
+import { useUser } from '@/hooks/user/login-logout';
+import { keysToCamelCase } from '@/lib/api/utils';
+import {
+	AccountCard,
+	type CreateAccountFormData,
+} from '../landing/user-forms/account';
 
 export const AccountSettings: FC = () => {
-	return <CreateAccountCard />;
+	const { user } = useUser();
+
+	return (
+		<AccountCard
+			mode="edit"
+			defaultValues={keysToCamelCase({
+				...user,
+				income:
+					user?.income && !Number.isNaN(parseInt(user.income))
+						? parseInt(user.income)
+						: undefined,
+				// password: undefined,
+			} as Partial<CreateAccountFormData>)}
+		/>
+	);
 };
