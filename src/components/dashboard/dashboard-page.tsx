b12b172ed356c877ba1c4d3dde10cdb0aaaeb417
sereno <serenomdominguez@gmail.com>
2025-11-06 00:11:21 -0500

    transactions table

--- src/components/dashboard/dashboard-page.tsx
+++ src/components/dashboard/dashboard-page.tsx
@@ -5,21 +5,27 @@ import {
 	useItems,
 	useTransactions,
 } from '@/hooks/budget';
+import { TransactionsTable } from './transactions-table';
 
 export const DashboardPage: FC = () => {
-	const { categories } = useCategories();
-	const { items } = useItems();
-	const { accounts } = useAccounts();
-	const { transactions } = useTransactions();
+	// const { categories } = useCategories();
+	// const { items } = useItems();
+	// const { accounts } = useAccounts();
+	// const { transactions } = useTransactions();
 
-	useEffect(() => {
-		console.log({
-			categories,
-			items,
-			accounts,
-			transactions,
-		});
-	}, [categories, items, accounts, transactions]);
+	// useEffect(() => {
+	// 	console.log({
+	// 		categories,
+	// 		items,
+	// 		accounts,
+	// 		transactions,
+	// 	});
+	// }, [categories, items, accounts, transactions]);
 
-	return <p>Dashboard</p>;
+	// return <p>ok</p>;
+	return (
+		<div>
+			<TransactionsTable />
+		</div>
+	);
 };
